ba78e21de325173698f685f283f26b3faf9c2047
dehio3 <dehitomo0318@gmail.com>
2019-10-03 14:37:21 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,15 @@
 module "ec2_test" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "~> v2.0"
-  name                   = "ec2-test"
+  name                   = "ec2-circleci"
   instance_count         = 1
-  ami                    = "ami-0b8d0d6ac70e5750c" #amzn2-ami-hvm-2.0.20181114-x86_64-ebs
+  ami                    = "ami-0ebe863c3d16bca9d" #amzn2-ami-hvm-2.0.20190618-x86_64-ebs
   instance_type          = "t2.micro"
   key_name               = "test-key"
   monitoring             = false
-  vpc_security_group_ids = ["sg-72fa6f08"]
-  subnet_id              = "subnet-2a324a4f"
+  vpc_security_group_ids = ["sg-b79875d3"]
+  subnet_id              = "subnet-29f3b85e"
   tags = {
-    Name = "ec2-test"
     Terraform = "true"
     Environment = "dev"
   }
